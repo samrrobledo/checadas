a48b0bcb8b3fd0c08b8b7311270620aaa9a93231
samrrobledo <sam.rrobledo@gmail.com>
2023-01-21 18:52:06 -0600

    Codigo limpio

--- checadas/Program.cs
+++ checadas/Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-for(; ; )Console.WriteLine("Hello, World!"); ;
-
-<<<<<<< HEAD
-=======
-//sam.rrobledo@gmail.com
->>>>>>> mail
+for(; ; )Console.WriteLine("Hello, World!");
