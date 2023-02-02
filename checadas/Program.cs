9e8e7a72789433960b76b011593dafc9c71cbbac
samrrobledo <sam.rrobledo@gmail.com>
2023-02-01 19:43:32 -0600

    delete all

--- checadas/Program.cs
+++ checadas/Program.cs
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 
 for(; ; )Console.WriteLine("Hello, World!");
 
 //sam.rrobledo@gmail.com
+=======
+for(; ; )Console.WriteLine("Hello, World!");
+>>>>>>> 776751f (delete all)
